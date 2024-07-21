9c0b4100d2e93cfc3299ee44609bd92467879c00
Sefa İlyas Öz <sefailyas1455@gmail.com>
2024-07-21 23:33:04 +0300

    list item animation update

--- src/Components/BudgetItem.tsx
+++ src/Components/BudgetItem.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { Budget } from "../Utils/types";
-import { useIsPresent, motion, useMotionValue, useTransform } from "framer-motion";
+import { motion, usePresence } from "framer-motion";
 import moment from "moment";
 import { TrashIcon } from "@heroicons/react/24/solid";
 import { useBudget } from "../Hooks/useBudgets";
@@ -11,24 +10,29 @@ type Props = {
 
 const BudgetItem = ({ item }: Props) => {
 	const { deleteItem } = useBudget();
+	const [isPresent, safeToRemove] = usePresence();
+	const transition = { type: "spring", stiffness: 500, damping: 50, mass: 1 };
 	const animations = {
-		initial: { scale: 0, opacity: 0 },
-		animate: { scale: 1, opacity: 1 },
-		exit: { scale: 0, opacity: 0 },
-		transition: { type: "spring", stiffness: 750, damping: 100, bounce: 0.5 },
+		layout: true,
+		initial: "out",
+		style: {},
+		animate: isPresent ? "in" : "out",
+		whileTap: "tapped",
+		variants: {
+			in: { scaleY: 1, opacity: 1 },
+			out: { scaleY: 0, opacity: 0, zIndex: -1 },
+			tapped: { scale: 0.98, opacity: 0.5, transition: { duration: 0.1 } },
+		},
+		onAnimationComplete: () => !isPresent && safeToRemove(),
+		transition,
 	};
 	const handleDelete = () => deleteItem(item.id);
 	return (
 		<motion.div
-			layout
-			initial='initial'
-			animate='animate'
-			exit='exit'
-			transition={animations.transition}
-			variants={animations}
+			{...animations}
 			className={`border-l-8 border rounded-lg budget-item font-medium my-5 justify-between py-1 flex flex-row px-2 w-11/12 mx-auto ${
 				item.type === "expense" ? "border-l-red-600 border-red-600" : "border-l-green-600 border-green-600"
-			}`}>
+			} ${isPresent ? "static" : "absolute"}`}>
 			<div className='flex flex-col relative'>
 				<span className='text-xl text-colors'>{item.description}</span>
 				<span className='text-lg text-colors'>${Math.abs(item.amount)}</span>
